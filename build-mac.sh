b90782cf7ba98a1fbafcc6be0a9d81834ae4e34c
Pemba Sherpa <norsangpemba@gmail.com>
2024-11-21 16:32:39 -0500

    bundle ffmpeg

--- build-mac.sh
+++ build-mac.sh
@@ -9,7 +9,7 @@ MAIN_SCRIPT="src/FreeScribe.client/client.py"
 ICON_PATH="src/FreeScribe.client/assets/logo.ico"
 
 # Run PyInstaller to create the standalone executable
-pyinstaller --additional-hooks-dir=./scripts/hooks --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" "$MAIN_SCRIPT" --windowed
+pyinstaller --additional-hooks-dir=./scripts/hooks --add-data "./src/FreeScribe.client/whisper-assets:whisper/assets" --add-data "./src/FreeScribe.client/markdown:markdown" --add-data "./src/FreeScribe.client/assets:assets" --add-binary "/usr/local/bin/ffmpeg:ffmpeg" --name "$EXECUTABLE_NAME" --icon="$ICON_PATH" "$MAIN_SCRIPT" --windowed
 
 # Print a message indicating that the build is complete
 echo "Build complete. Executable created: $DIST_PATH/$EXECUTABLE_NAME.app"
