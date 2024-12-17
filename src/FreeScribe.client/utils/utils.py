355e08b211e8b29675b89c57a15e24a20784428f
Pemba Sherpa <norsangpemba@gmail.com>
2024-12-17 11:30:15 -0500

    for win

--- src/FreeScribe.client/utils/utils.py
+++ src/FreeScribe.client/utils/utils.py
@@ -1,5 +1,6 @@
 
 import ctypes
+import sys
 
 # Define the mutex name and error code
 MUTEX_NAME = 'Global\\FreeScribe_Instance'
@@ -15,11 +16,15 @@ def window_has_running_instance() -> bool:
     Returns:
         bool: True if another instance is running, False otherwise
     """
-    global mutex
+    if sys.platform == 'win32':
+        global mutex
 
-    # Create a named mutex
-    mutex = ctypes.windll.kernel32.CreateMutexW(None, False, MUTEX_NAME)
-    return ctypes.windll.kernel32.GetLastError() == ERROR_ALREADY_EXISTS
+        # Create a named mutex
+        mutex = ctypes.windll.kernel32.CreateMutexW(None, False, MUTEX_NAME)
+        return ctypes.windll.kernel32.GetLastError() == ERROR_ALREADY_EXISTS
+    elif sys.platform == 'darwin':
+        # TODO - Implement for macOS
+        pass
 
 def bring_to_front(app_name: str):
     """
@@ -28,19 +33,23 @@ def bring_to_front(app_name: str):
         app_name (str): The name of the application window to bring to the front
     """
 
-    # TODO - Check platform and handle for different platform
-    U32DLL = ctypes.WinDLL('user32')
-    SW_SHOW = 5
-    hwnd = U32DLL.FindWindowW(None, app_name)
-    U32DLL.ShowWindow(hwnd, SW_SHOW)
-    U32DLL.SetForegroundWindow(hwnd)
+    if sys.platform == 'win32':
+        U32DLL = ctypes.WinDLL('user32')
+        SW_SHOW = 5
+        hwnd = U32DLL.FindWindowW(None, app_name)
+        U32DLL.ShowWindow(hwnd, SW_SHOW)
+        U32DLL.SetForegroundWindow(hwnd)
+    elif sys.platform == 'darwin':
+        # TODO - Implement for macOS
+        pass
 
 def close_mutex():
     """
     Close the mutex handle to release the resource.
     """
-    global mutex
-    if mutex:
-        ctypes.windll.kernel32.ReleaseMutex(mutex)
-        ctypes.windll.kernel32.CloseHandle(mutex)
-        mutex = None
+    if sys.platform == 'win32':
+        global mutex
+        if mutex:
+            ctypes.windll.kernel32.ReleaseMutex(mutex)
+            ctypes.windll.kernel32.CloseHandle(mutex)
+            mutex = None
